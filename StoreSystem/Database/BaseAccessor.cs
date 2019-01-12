5965490809d49cea6eecbba976a32c7e7e8a2da9
a.e <ak000en@yahoo.co.jp>
2019-01-13 01:29:59 +0900

    add BaseAccessor

---
+++ StoreSystem/Database/BaseAccessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace StoreSystem.Database
+{
+    public abstract class BaseAccessor : IDisposable
+    {
+        // SQLコネクション
+        private SqlConnection con;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- StoreSystem/Program.cs
+++ StoreSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace StoreSystem
 {
@@ -10,8 +11,30 @@ namespace StoreSystem
     {
         static void Main(string[] args)
         {
-            // 
-            // 
+            TestDBConnection();
+
+            Console.ReadKey();
+        }
+
+        private static void TestDBConnection()
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user01\source\repos\Practice\StoreSystem\Database1.mdf;Integrated Security=True";
+
+            string sql = "select * from Product";
+            using (var con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (var cmd = new SqlCommand(sql, con))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["Id"]}, {reader["Name"]}, {reader["Price"]}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
